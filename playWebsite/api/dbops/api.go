8c641f6183b90868f950687c7e82b33a753b2f63
cicada <1713856662a@gmail.com>
2020-02-14 22:28:23 +0800

    处理数据库exec错误

--- playWebsite/api/dbops/api.go
+++ playWebsite/api/dbops/api.go
@@ -1,40 +1,50 @@
 package dbops
 
 import (
+	"database/sql"
 	"log"
 )
 
 func AddUserCredential(loginName string, pwd string) error{
 	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, password) VALUES (?, ?)")
+	defer stmtIns.Close()
 	if err != nil{
 		return err
 	}
-	stmtIns.Exec(loginName, pwd)
-	stmtIns.Close()
+	_, err = stmtIns.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func GetUserCredential(loginName string) (string, error){
 	stmtOut, err := dbConn.Prepare("SELECT password FROM users WHERE login_name = ?")
-	//defer stmtOut.Close()  // 在堆栈退出的时候才会调用 稍微会损耗一些性能
+	defer stmtOut.Close()  // 在堆栈退出的时候才会调用 稍微会损耗一些性能
 	if err != nil{
 		log.Println(err)
 		return "", err
 	}
 	var pwd string
-	stmtOut.QueryRow(loginName).Scan(&pwd)
-	stmtOut.Close()
+	err = stmtOut.QueryRow(loginName).Scan(&pwd)
+	if err != nil && err != sql.ErrNoRows{
+		return "", err
+	}
+	//stmtOut.Close()
 	return pwd, nil
 }
 
 func DeleteUser(loginName string, pwd string) error {
 	stmtDel, err := dbConn.Prepare("DELETE FROM users where login_name=? AND password=?")
+	defer stmtDel.Close()
 	if err != nil{
 		log.Println(err)
 		return err
 	}
-	stmtDel.Exec(loginName, pwd)
-	stmtDel.Close()
+	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
