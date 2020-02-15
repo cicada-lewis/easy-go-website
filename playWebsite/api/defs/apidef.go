b8980754ac10bf6b72369571b731f2b3c5292140
cicada <1713856662a@gmail.com>
2020-02-15 17:30:39 +0800

    数据声明

--- playWebsite/api/defs/apidef.go
+++ playWebsite/api/defs/apidef.go
@@ -1,6 +1,21 @@
 package defs
 
+import (
+	"time"
+)
+
 type UserCredential struct {
 	Username string `json:"user_name"`
 	Pwd string `json:"pwd"`
 }
+
+// data model
+
+type VideoInfo struct {
+	VideoId string
+	AuthorId int
+	Name string
+	DisplayCtime string
+	CreateTime time.Time
+}
+
