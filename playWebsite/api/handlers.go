37a66f4479e685740761748bd6eee28fcc12ace5
cicada <1713856662a@gmail.com>
2020-02-19 08:25:21 +0800

    实现用户登录逻辑

--- playWebsite/api/handlers.go
+++ playWebsite/api/handlers.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/Catelemmon/easy-go-website/playWebsite/api/dbops"
+	"github.com/Catelemmon/easy-go-website/playWebsite/api/defs"
+	"github.com/Catelemmon/easy-go-website/playWebsite/api/session"
 	"github.com/julienschmidt/httprouter"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
 
 func CreateUser(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	writer.Write([]byte("Create User Handler"))
-	io.WriteString(writer, "Create User Handler")
-	return
+	 res, _ := ioutil.ReadAll(request.Body)
+	 ubody := &defs.UserCredential{}
+
+	 if err := json.Unmarshal(res, ubody); err != nil {
+	 	sendErrorResponse(writer, defs.ErrorResponseBodyParseFailed)
+	 	return
+	 }
+	 if err := dbops.AddUserCredential(ubody.Username, ubody.Pwd); err != nil {
+	 	sendErrorResponse(writer, defs.ErrorDBError)
+	 }
+	 sid := session.GenerateNewSessionId(ubody.Username)
+	 su := &defs.SignedUp{Success: true, SessionId: sid}
+	 if resp, err := json.Marshal(su); err != nil {
+		return
+	} else {
+		sendNormalResponse(writer, string(resp), 200)
+	 }
 }
 
 func LoginUser(writer http.ResponseWriter, request *http.Request, params httprouter.Params)  {
