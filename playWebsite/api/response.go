951760b347bcc90d361440c917b11da8b31ab969
cicada <1713856662a@gmail.com>
2020-02-19 08:28:04 +0800

    复用响应逻辑

--- playWebsite/api/response.go
+++ playWebsite/api/response.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/Catelemmon/easy-go-website/playWebsite/api/defs"
+	"io"
 	"net/http"
 )
 
 
 
-func sendErrorResponse(writer http.ResponseWriter) {
-
+func sendErrorResponse(writer http.ResponseWriter, errResp defs.ErrResponse) {
+	writer.WriteHeader(errResp.HttpSc)
+	resStr, _ := json.Marshal(&errResp.Error)
+	io.WriteString(writer, string(resStr))
 }
 
-func sendNormalResponse(writer http.ResponseWriter){
-
+func sendNormalResponse(writer http.ResponseWriter, resp string, sc int){
+	writer.WriteHeader(sc)
+	io.WriteString(writer, resp)
 }
